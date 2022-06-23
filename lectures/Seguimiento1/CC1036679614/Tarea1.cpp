8dfbe64c893bd5000851ed41de10e1a2fefad414
EstebanM_98 <esteban.marulandaa@udea.edu.co>
2022-06-23 08:25:26 -0500

    Tarea1 modificada

--- lectures/Seguimiento1/CC1036679614/Tarea1.cpp
+++ lectures/Seguimiento1/CC1036679614/Tarea1.cpp
@@ -13,7 +13,7 @@ int main()
   int T_a{0};
   
   cout << "Calculo de la tasa de calor transferido. Ingrese el número de acuerdo a la superficie de interes:" << endl;
-  cout << "1. Area rectangular"<< endl;
+  cout << "1. Area rectangular (chip)"<< endl;
   cout << "2. Area eliptica" << endl;
   cout << "3. Otra" << endl;
   cin >> var;
@@ -32,9 +32,9 @@ int main()
     float ancho{0};
     cout << "Ingrese largo del rectangulo en metros: " << endl;
     cin >> largo;
-    cout << "Ingrese ancho del rectangulo" << endl;
+    cout << "Ingrese ancho del rectangulo en metros" << endl;
     cin >> ancho;
-    cout << "La tasa de calor transferido es: "<< (T_s-T_a)*largo*ancho*AIRCONV << " watts";
+    cout << "La tasa de calor transferido al chip: "<< (T_s-T_a)*largo*ancho*AIRCONV << " watts";
   }
   if (var=="2"){
     float semi_mayor{0};
@@ -52,7 +52,13 @@ int main()
     cin >> Area;
     cout << "La tasa de calor transferido es: "<< Area*AIRCONV*(T_s-T_a) << " watts";
   }
-  
+
+  if (var=="4"){
+    float Area{0};
+    cout << "Ingrese el area en metros: " << endl;
+    cin >> Area;
+    cout << "La tasa de calor transferido es al chip: "<< Area*AIRCONV*(T_s-T_a) << " watts";
+  }
   return 0;
 
    }
