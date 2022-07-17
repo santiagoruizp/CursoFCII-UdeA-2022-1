fd1873c7a8b0bdf0516c508262079b276967f124
santiagoruizp <42008840+santiagoruizp@users.noreply.github.com>
2022-07-16 20:04:57 -0500

    Parcial #1

---
+++ lectures/Parcial1/CC1036785977/Punto2.cpp
@@ -0,0 +1,52 @@
+/*
+Santiago Ruiz Piedrahita
+Parcial #1 
+Punto #2
+
+Este codigo mira si un numero es perfecto y calcula los numero perfectos hasta un numero N muy grande
+*/
+#include <iostream>
+
+using namespace std;
+
+// Definimos la funcion numero perfecto
+
+int perfecto(int x)
+
+{
+    int suma = 0;
+    
+    for (int i=1 ; i<x ; i++)
+    {
+        if  ((x % i)==0)
+            suma += i;
+        
+    }
+    
+    if (suma==x) 
+    {
+        cout << x << " Es numero perfecto" << endl;
+        cout << "Divisores:" << endl;
+        for (int j=1 ; j<x ; j++)
+        {
+            if  ((x % j)==0)
+                cout << j << endl;
+        }
+        cout <<" "<< endl;
+        return 1; // Es perfecto
+    }    
+    else 
+        return 0; // NO Es perfecto
+}
+
+// funcion main
+int main()
+{
+    int iteraciones(100000); // hasta que numero se buscan los perfectos
+    for (int k=1 ; k<=iteraciones ; k++)
+    {
+        perfecto(k);
+    }
+    return 0;
+}
+
